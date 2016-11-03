35c922952fe5548657a7304a9e752b307399e027
Brett Yukich <brett.yukich@fostermoore.com>
2016-11-04 11:52:16 +1300

    use subcommands instead of options

--- spend.py
+++ spend.py
@@ -1,33 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from peewee import *
 import argparse
 import datetime
-
-# TODO everything
-
-# spend
-
-# command options
-# --set-income <amount> <period>
-# --set-monthly-income <amount> 30
-# --set-weekly-income <amount> 7
-# --set-daily-income <amount> 1
-
-# --add-bill <amount> <period>
-# --add-montly-bill <amount> 30
-# --add-weekly-bill <amount> 7
-# --add-daily-bill <amount> 1
-# note: add savings as bills
-
-# --record-spend <amount>
-
-# --show
-# note: ouput graph of spending over last 30 days
-
-# spn_description | spn_amount | spn_date
-# bll_description | bll_amount | bll_period
-# inc_description | inc_amount | inc_period
 
 db = SqliteDatabase('spend.db')
 
@@ -52,6 +27,7 @@ class Bill(Model):
     def daily_amount(self):
         return self.amount / self.ieriod
 
+
 class Spend(Model):
     amount = DoubleField()
     date = DateTimeField(default=datetime.datetime.now)
@@ -59,41 +35,41 @@ class Spend(Model):
     class Meta:
         database = db
 
+
 db.connect()
 if not Income.table_exists():
     db.create_tables([Income, Bill, Spend])
 
+
+class PositionalAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        print('args: ', values)
+
+
 class AddIncomeAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
-        print('%r %r %r' % (namespace, values, option_string))
-
         income = Income.create(amount=values[0], period=values[1])
         income.save()
 
-class AddBillAction(argparse.Action):
+
+class ListIncomeAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
-        print('%r %r %r' % (namespace, values, option_string))
-
-        bill = Bill.create(amount=values[0], period=values[1])
-        bill.save()
-
-class SpendAction(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-        print('%r %r %r' % (namespace, values, option_string))
-
-        spend = Spend.create(amount=values[0])
-        spend.save()
-
-class PrintAction(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-
         print('Income:')
         total_daily_income = 0.0
         for income in Income.select():
             total_daily_income += income.daily_amount()
             print(income.amount, income.period)
         print('Total daily income: ', total_daily_income)
-        
+
+
+class AddBillAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        bill = Bill.create(amount=values[0], period=values[1])
+        bill.save()
+
+
+class ListBillAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
         print('Bill:')
         total_daily_bill = 0.0
         for bill in Bill.select():
@@ -101,13 +77,59 @@ class PrintAction(argparse.Action):
             print(bill.amount, bill.period)
         print('Total daily bill: ', total_daily_bill)
 
+
+class AddSpendAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        if len(values) > 0:
+            spend = Spend.create(amount=values[0])
+            spend.save()
+
+
+class ListSpendAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
         print('Spend:')
         for spend in Spend.select():
-            print(spend.amount, spend.date)
+            print(spend.id, spend.amount, spend.date)
 
-parser = argparse.ArgumentParser(description='Process commandline input')
-parser.add_argument('--add-income', nargs=2, action=AddIncomeAction)
-parser.add_argument('--add-bill', nargs=2, action=AddBillAction)
-parser.add_argument('--spend', nargs=1, action=SpendAction)  # TODO allow user to enter date
-parser.add_argument('--print', nargs=0, action=PrintAction)
+
+class PrintAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+
+        listIncome = ListIncomeAction(option_string, dest=None)  # __init__
+        listIncome(parser, namespace, values, None)  # __call__
+
+        listBill = ListBillAction(option_string, dest=None)
+        listBill(parser, namespace, values, None)
+        
+        listSpend = ListSpendAction(option_string, dest=None)
+        listSpend(parser, namespace, values, None)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--print', nargs=0, action=PrintAction)
+
+subparsers = parser.add_subparsers(dest='args')
+
+# spend command
+spend_parser = subparsers.add_parser('spend', help='list spending')
+spend_parser.add_argument('list', nargs=0, action=ListSpendAction)
+spend_parser.add_argument('-a', '--add', nargs=1, action=AddSpendAction)
+spend_parser.add_argument('-d', '--delete', nargs=1)
+
+# income command
+income_parser = subparsers.add_parser('income', help='list income')
+income_parser.add_argument('list', nargs=0, action=ListIncomeAction)
+income_parser.add_argument('-a', '--add',  nargs=2, action=AddIncomeAction)
+income_parser.add_argument('-d', '--delete', nargs=1)
+
+# bill command
+bill_parser = subparsers.add_parser('bill', help='list bill')
+bill_parser.add_argument('list', nargs=0, action=ListBillAction)
+bill_parser.add_argument('-a', '--add', nargs=2, action=AddBillAction)
+bill_parser.add_argument('-d', '--delete', nargs=1)
+
+import_parser = subparsers.add_parser('import')
+import_parser.add_argument(file, nargs=1)
+improt_parser.add_argument('-f', '--format', nargs=1)
+
 args = parser.parse_args()
